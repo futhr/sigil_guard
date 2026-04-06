1647862ec64ce8f74b316f1f2b1b4517299f4615
Tobias Bohwalli <hi@futhr.io>
2026-04-06 10:29:47 +0200

    chore: update NIF precompiled checksums for v0.1.1

--- checksum-Elixir.SigilGuard.Backend.NIF.Native.exs
+++ checksum-Elixir.SigilGuard.Backend.NIF.Native.exs
@@ -1,2 +1,10 @@
 %{
+  "libsigil_guard_nif-v0.1.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:a7f422be1150cd37769f1788912849a99bbb0ee19b56954ea8d0b8382a5d0f0c",
+  "libsigil_guard_nif-v0.1.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:81eec439c5c5a320647590932567ea59cb681ebc366cb3e0ccf0d5624d77d6fa",
+  "libsigil_guard_nif-v0.1.1-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:7b043dbcfaa600de05da301eeba583bb4762fb9fb3c0d09ceaee1aeffa670a86",
+  "libsigil_guard_nif-v0.1.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:b5ed0922c6042614d1dd91572eb8dfca0bda9209cd2daa83f182f696b0d1929e",
+  "libsigil_guard_nif-v0.1.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:b47c698926ea34c92b4255a201363da8bb04b120268100544255d22a48a496b4",
+  "libsigil_guard_nif-v0.1.1-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:74c03e992a2ad6d8d364addc07002aca66b4b6024d0a35c13a2d19c5123615e8",
+  "sigil_guard_nif-v0.1.1-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:296bda82f2541062dd05df395378632dc31fbecbbaf78b8ad5da5e49dc4d4adc",
+  "sigil_guard_nif-v0.1.1-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:5060bfc1d20a7839ca930691246fee2f02f7c6af147702e3841124307013e9fc",
 }
