eceb5b7cd924450f564e11ea3df69a3d8542365a
futhr <hi@futhr.io>
2026-06-10 13:40:06 +0200

    chore: update NIF checksums for v0.2.0 binaries

--- checksum-Elixir.SigilGuard.Backend.NIF.Native.exs
+++ checksum-Elixir.SigilGuard.Backend.NIF.Native.exs
@@ -1,10 +1,10 @@
 %{
-  "libsigil_guard_nif-v0.1.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:a7f422be1150cd37769f1788912849a99bbb0ee19b56954ea8d0b8382a5d0f0c",
-  "libsigil_guard_nif-v0.1.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:81eec439c5c5a320647590932567ea59cb681ebc366cb3e0ccf0d5624d77d6fa",
-  "libsigil_guard_nif-v0.1.1-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:7b043dbcfaa600de05da301eeba583bb4762fb9fb3c0d09ceaee1aeffa670a86",
-  "libsigil_guard_nif-v0.1.1-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:b5ed0922c6042614d1dd91572eb8dfca0bda9209cd2daa83f182f696b0d1929e",
-  "libsigil_guard_nif-v0.1.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:b47c698926ea34c92b4255a201363da8bb04b120268100544255d22a48a496b4",
-  "libsigil_guard_nif-v0.1.1-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:74c03e992a2ad6d8d364addc07002aca66b4b6024d0a35c13a2d19c5123615e8",
-  "sigil_guard_nif-v0.1.1-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:296bda82f2541062dd05df395378632dc31fbecbbaf78b8ad5da5e49dc4d4adc",
-  "sigil_guard_nif-v0.1.1-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:5060bfc1d20a7839ca930691246fee2f02f7c6af147702e3841124307013e9fc",
+  "libsigil_guard_nif-v0.2.0-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:eba1870c00ef159b41946e27b51b6338182d3a7ed83f8c396662cc3b22f6be0c",
+  "libsigil_guard_nif-v0.2.0-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:3a3b5a3578b81213c8a43aa5581b96429e7f33a834f76477c88447070baf3561",
+  "libsigil_guard_nif-v0.2.0-nif-2.15-aarch64-unknown-linux-musl.so.tar.gz" => "sha256:0a297c46fec3a0a840726f584764cdf1594db97876ba9150520cf08b21b96c22",
+  "libsigil_guard_nif-v0.2.0-nif-2.15-x86_64-apple-darwin.so.tar.gz" => "sha256:69b43dc9a2c537a8e29dfc0693137b8a1d240384eb165ea8e06ce8887c150444",
+  "libsigil_guard_nif-v0.2.0-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:66350eb03a5dcd55b636ee42049195e4dd1c99bc6a7784faac6354b9526e80e6",
+  "libsigil_guard_nif-v0.2.0-nif-2.15-x86_64-unknown-linux-musl.so.tar.gz" => "sha256:dce02dd8dded5fe235c01f6b47504983b0607d594f15bf221520925b17f53ee0",
+  "sigil_guard_nif-v0.2.0-nif-2.15-x86_64-pc-windows-gnu.dll.tar.gz" => "sha256:636c5ef070ec83f281928dea6ecce33fa31d9adc3c4a3e8ac106df232fcc16b4",
+  "sigil_guard_nif-v0.2.0-nif-2.15-x86_64-pc-windows-msvc.dll.tar.gz" => "sha256:88080bd34c00d3ca03f661d206e52b9d1e8f7828bcf3da94bf1ebdb436c6f609",
 }
